42b0ba84aa5dcf96f2a90bb81325c343fa22cb22
kelxpad <felouise2006@gmail.com>
2026-03-18 21:36:18 +0800

    block cut tree impl

---
+++ lab5/blockcuttree.py
@@ -0,0 +1,88 @@
+class BlockCutTree:
+    def __init__(self, n: int) -> None:
+        self.n = n
+        self.adj = [[] for _ in range(n)]
+
+        # DFS state
+        self.disc = [-1] * n
+        self.low = [0] * n
+        self.time = 0
+
+        # stack of edges (u, v)
+        self.edge_stack = []
+
+        # results
+        self.bccs = [] # list of list of edges
+
+        self.bct_adj = []
+
+    def add_edge(self, u, v):
+        if u == v:
+            self.bccs.append([(u, u)])
+        else:
+            self.adj[u].append(v)
+            self.adj[v].append(u)
+
+    def dfs(self, u, parent):
+        self.disc[u] = self.low[u] = self.time
+        self.time += 1
+
+        for v in self.adj[u]:
+            # self.loop
+            if v == u:
+                continue
+
+            if self.disc[v] == -1:
+                # tree edge
+                self.edge_stack.append((u, v))
+
+                self.dfs(v, u)
+
+                self.low[u] = min(self.low[u], self.low[v])
+
+                # BCC condition
+                if self.low[v] >= self.disc[u]:
+                    bcc = []
+                    while True:
+                        e = self.edge_stack.pop()
+                        bcc.append(e)
+                        if e == (u, v) or e == (v, u):
+                            break
+                    self.bccs.append(bcc)
+            
+            elif v != parent and self.disc[v] < self.disc[u]:
+                # back edge (important: avoid duplicates)
+                self.edge_stack.append((u, v))
+                self.low[u] = min(self.low[u], self.disc[v])
+
+    def build(self):
+        """
+        computes all BCCs and builds the BCT
+        rets adjacency list of the block-cut tree
+        """
+
+        # find BCCs by running DFS on all comps
+        for i in range(self.n):
+            if self.disc[i] == -1:
+                self.dfs(i, -1)
+        
+        # build block-cut tree
+        # nodes 0..n--1: original node
+        # n..n+len(bccs)-1: BCC nodes
+        total_nodes = self.n + len(self.bccs)
+        bct_adj = [[] for _ in range(total_nodes)]
+
+        for i, bcc in enumerate(self.bccs):
+            bcc_node = self.n + i
+            vertices = set()
+
+            for u, v in bcc:
+                vertices.add(u)
+                vertices.add(v)
+
+            for v in vertices:
+                bct_adj[v].append(bcc_node)
+                bct_adj[bcc_node].append(v)
+        
+        self.bct_adj = bct_adj
+        return bct_adj
